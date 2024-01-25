f5777bd77d9dce15ca592aa0b78e0b3e2682e277
Samuel Chu <samueljchu0126@gmail.com>
2024-01-25 03:22:50 -0500

    dom modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your code here!
+// index.js
+
+// Get the main element
+var mainElement = document.getElementById('main');
+
+// Remove the main element if it exists
+if (mainElement) {
+  mainElement.parentNode.removeChild(mainElement);
+}
+
+// Create a new <h1> element
+var newHeader = document.createElement("h1");
+
+// Set the id attribute of the newHeader to 'victory'
+newHeader.id = 'victory';
+
+// Set the text content of the newHeader
+newHeader.textContent = "SAMUEL-CHU is the champion";
+
+// Append the newHeader to the body
+document.body.appendChild(newHeader);
