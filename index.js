17d65d4a63c36200863668b32f1c8e8f847ec40a
Samuel Chu <samueljchu0126@gmail.com>
2024-02-03 17:18:09 -0500

    DOM Mods

--- index.js
+++ index.js
@@ -1,21 +1,7 @@
-// index.js
+main.remove();
 
-// Get the main element
-var mainElement = document.getElementById('main');
+const newHeader = document.createElement("h1");
+  newHeader.id = 'victory';
+  newHeader.textContent = "SAMUEL-CHU is the champion";
+  document.body.append(newHeader);
 
-// Remove the main element if it exists
-if (mainElement) {
-  mainElement.parentNode.removeChild(mainElement);
-}
-
-// Create a new <h1> element
-var newHeader = document.createElement("h1");
-
-// Set the id attribute of the newHeader to 'victory'
-newHeader.id = 'victory';
-
-// Set the text content of the newHeader
-newHeader.textContent = "SAMUEL-CHU is the champion";
-
-// Append the newHeader to the body
-document.body.appendChild(newHeader);
